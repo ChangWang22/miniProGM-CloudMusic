f13a52167405938fc18019098787edc7269df1a2
lixu <lixu@qq.com>
2021-07-16 10:13:54 +0800

    初始

---
+++ component/navHeader/navheader.js
@@ -0,0 +1,30 @@
+// component/navHeader/navheader.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    title:{
+      type:String,
+      value:'标题默认值'
+    },
+    desc:{
+      type:String,
+      value:'描述默认值'
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
